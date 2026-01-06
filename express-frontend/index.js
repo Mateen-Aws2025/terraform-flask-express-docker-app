90c16fee2d0c4debc039882626327ee67b5b360d
EC2 Default User <ec2-user@ip-172-31-66-7.ec2.internal>
2026-01-06 20:28:36 +0000

    Fix frontend backend call via ALB routing

--- express-frontend/index.js
+++ express-frontend/index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 
 app.post('/submit', async (req, res) => {
   try {
-    const response = await fetch('http://flask-express-alb/api/submit', {
+    const response = await fetch("http://localhost/api/submit", {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(req.body)
